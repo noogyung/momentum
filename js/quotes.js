9ed6e359c6f819a01f71f91d871abb0affd6feec
noogyung <37189970+noogyung@users.noreply.github.com>
2022-01-20 21:54:37 +0900

    7 todo

--- js/quotes.js
+++ js/quotes.js
@@ -1,4 +1,30 @@
-const quotes = [{}];
+// const axios = require("axios");
+// const cherrio = require("cheerio");
+
+// const quoteList ={
+//     'quote':'',
+//     'author':''
+// }
+
+// function getData(){
+//     request("http://www.quotationspage.com/random.php", function(err, res, body){
+//         const $ = cherrio.load(body);
+//         const bodyList = $(".content dl").map(function (i, element){
+//             quoteList[quote] = String($(element).find("dl:nth-of-type(1)").text());
+//             quoteList[author] = String($(element).find("dl:nth-of-type(2)").text());
+
+//             console.log(quoteList);
+//         });
+//     });
+// }
+
+// getData();
+
+
+const quotes = [{
+    quote:"tatdstsdtatsdst",
+    author:"stststs"
+}];
 
 const quote = document.querySelector("#quote span:first-child");
 const author = document.querySelector("#quote span:last-child");
@@ -7,4 +33,3 @@ const todaysQuote = quotes[Math.floor(Math.random() * quotes.length)];
 
 quote.innerText = todaysQuote.quote;
 author.innerText = todaysQuote.author;
-
